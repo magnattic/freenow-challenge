afa7f1c68ee60610e7d6eef1d0b5ad962c586327
magnattic <atticae@gmail.com>
2025-06-10 17:29:15 +0200

    container layout

--- FE/src/App.tsx
+++ FE/src/App.tsx
@@ -4,23 +4,39 @@ import { useVehicles } from './api/useVehicles';
 import { useState } from 'react';
 import { VehicleList } from './domains/vehicles/VehicleList';
 import { Pagination } from './domains/vehicles/Pagination';
+import { MediaQueries } from '@freenow/wave';
+import styled from 'styled-components';
+
+const Container = styled.div`
+	display: grid;
+	${MediaQueries.small} {
+		grid-template-columns: 1fr;
+		grid-template-rows: auto 1fr auto;
+		gap: 16px;
+	}
+	${MediaQueries.medium} {
+		grid-template-columns: 1fr 3fr;
+		grid-template-rows: auto 1fr auto;
+		gap: 16px;
+	}
+`;
 
 function App() {
 	const [page, setPage] = useState(1);
 	const { data: vehicles, totalItems, pageSize } = useVehicles({ page });
 	return (
-		<>
+		<Container>
+			<VehicleMap vehicles={vehicles} onVehicleSelect={() => {}} />
 			<div>
-				<VehicleMap vehicles={vehicles} onVehicleSelect={() => {}} />
+				<VehicleList vehicles={vehicles} />
 				<Pagination
 					value={page}
 					onPageChange={(page) => setPage(page)}
 					totalItems={totalItems}
 					pageSize={pageSize}
 				/>
-				<VehicleList vehicles={vehicles} />
 			</div>
-		</>
+		</Container>
 	);
 }
 
